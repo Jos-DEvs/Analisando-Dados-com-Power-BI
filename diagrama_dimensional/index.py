13c77c3ca538d42b65bf5552c2845bfd77b2eaa7
Jos-DEvs <105674636+Jos-DEvs@users.noreply.github.com>
2025-03-03 18:28:33 -0300

    Update index.py

--- diagrama_dimensional/index.py
+++ diagrama_dimensional/index.py
@@ -1,1 +1,28 @@
-diagrama_dimensional
+CREATE TABLE Data( 
+	idData int primary key,
+	data datetime);
+
+CREATE TABLE Curso( 
+	idCurso int primary key,
+	data datetime,
+	Nome_do_curso varchar(50),
+	Descrição varchar(50));
+
+CREATE TABLE Disciplina( 
+	idDisciplina int primary key,
+	data datetime,
+	nome_da_disciplina varchar(50),
+	Descrição varchar(50));
+
+CREATE TABLE Departamento( 
+	idDepartamento int primary key,
+	nome_do_departamento varchar(50),
+	campus varchar(50));
+
+CREATE TABLE Professor( 
+	idProfessor int primary key,
+	idData int REFERENCES DATA(idData),
+	idCurso int REFERENCES CURSO(idCurso),
+	idDisciplina int REFERENCES DISCIPLINA(idDisciplina),
+	idDepartamento int REFERENCES DEPARTAMENTO(idDepartamento) ,
+	Nome_completo varchar(50));
